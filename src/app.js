c3774776bff702845fe267f305e5f0e41ff4c739
MarcoPasta <Luca.Chiarelli@outlook.de>
2024-02-16 11:39:19 +0100

    update db seed, add cors middleware

--- src/app.js
+++ src/app.js
@@ -8,13 +8,21 @@ const app = express();
 const port = 3000;
 const path = "/app";
 
+// Erlaubt alle CORS Anfragen
+app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // macht den Pfad '/app/public' statisch, somit kann man auch ohne deren
 // Angabe darauf zugreifen.
 // Zugriff auf Karte mit bsp. localhost:3000/images/blue.png
 app.use(express.static(join(path, "public")));
 
-// Erlaubt alle CORS Anfragen
-app.use(cors())
 
 // Middleware welche das parsen von JSON Requests ermöglicht
 app.use(express.json());
